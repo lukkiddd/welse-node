2abcd61c594e7eccef30b53ba40f7efaa46b1b5a
Phasathorn Suwansri <arbacuz@gmail.com>
2017-07-02 20:51:47 +0700

    fixed url from sandbox to api

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.get('/', function(req, res){
 });
 
 app.get('/api/ihealth/user', function(req, res) {
-  var BASE_IHEALTH_URL = 'http://sandboxapi.ihealthlabs.com/openapiv2/user';
+  var BASE_IHEALTH_URL = 'https://api.ihealthlabs.com:8443/openapiv2/user';
 
   var url = BASE_IHEALTH_URL + '/' + req.query.userId + '/glucose.json/' +
     '?client_id=a0550986de68400d8c3707c911c93b95' +
@@ -54,7 +54,7 @@ app.get('/api/ihealth/user', function(req, res) {
 app.get('/api/ihealth/callback', function(req, res) {
   if(req.query.code) {
     var code = req.query.code;
-    var url = 'http://sandboxapi.ihealthlabs.com/OpenApiV2/OAuthv2/userauthorization/?client_id=a0550986de68400d8c3707c911c93b95&client_secret=dd6187e7fe85487abdb904535ee45365&grant_type=authorization_code&redirect_uri=https%3A%2F%2Fwelse-app.azurewebsites.net%2Fapi%2Fihealth%2Fcallback&code=' + code;
+    var url = 'https://api.ihealthlabs.com:8443/OpenApiV2/OAuthv2/userauthorization/?client_id=a0550986de68400d8c3707c911c93b95&client_secret=dd6187e7fe85487abdb904535ee45365&grant_type=authorization_code&redirect_uri=https%3A%2F%2Fwelse-app.azurewebsites.net%2Fapi%2Fihealth%2Fcallback&code=' + code;
     request(url, function (error, response, body) {
       var data = JSON.parse(body);
       var access_token = data.AccessToken;
