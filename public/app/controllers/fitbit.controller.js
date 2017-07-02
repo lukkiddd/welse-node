3590ee1ca9d0caed8356cd304b7717b0845c9a2b
Phasathorn Suwansri <arbacuz@gmail.com>
2017-07-02 23:51:52 +0700

    test callback pass back to angular

--- public/app/controllers/fitbit.controller.js
+++ public/app/controllers/fitbit.controller.js
@@ -9,10 +9,28 @@
 
 	function fitbitCtrl($state, $stateParams, $http, Auth) {
 
-		console.log($stateParams);
-		var data = $stateParams.data;
-		
-		console.log(JSON.parse(data));
+		if($stateParams.data) {
+			var data = JSON.parse($stateParams.data);
+
+			Auth.$onAuthStateChanged(function(authData) {
+				console.log(data);
+				console.log(data['activities-steps'])
+				if(authData) {
+					_.forEach(data['activities-steps'], function (val) {
+						var time = new Date(val.dateTime).getTime();
+						firebase.database().ref('/health/').child(authData.uid)
+							.push({
+								chartType: 'column',
+								isDanger: false,
+								name: 'Steps',
+								timestamp: time,
+								unit: 'steps',
+								value: val.value
+							});
+						});
+				}
+			});
+		}
 
 }
 })();
