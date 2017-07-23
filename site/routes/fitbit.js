85e63e117a98b8dee54d4e0bbe70f44458a1cb73
Phasathorn Suwansri <arbacuz@gmail.com>
2017-07-23 11:16:31 -0700

    add health data

--- site/routes/fitbit.js
+++ site/routes/fitbit.js
@@ -11,7 +11,7 @@ const client = new FitbitApiClient(config.CLIENT_ID, config.CLIENT_SECRET)
 
 router.get('/authorize',  (req, res) => {
 	const id = req.query.id;
-	res.redirect(client.getAuthorizeUrl('activity heartrate sleep', 'https://welse-node-v6.azurewebsites.net/api/fitbit/callback', 'none', id))
+	res.redirect(client.getAuthorizeUrl('activity sleep', 'https://welse-node-v6.azurewebsites.net/api/fitbit/callback', 'none', id))
 });
 
 router.get('/callback', (req, res) => {
@@ -21,27 +21,70 @@ router.get('/callback', (req, res) => {
 			client.get('/activities/steps/date/today/1y.json', result.access_token)
 				.then( async (results) => {
 					const steps = results[0]['activities-steps'];
-					const stepData = _.sortBy(_.takeRight(steps, 100), 'dateTime');
-					
-					_.forEach(stepData, async (val) => {
-						let date = new Date(val.dateTime).getTime() / 1000
-						let step = new Health({
-							chartType: 'column',
-							name: 'steps',
-							timestamp: date,
-							unit: 'step',
-							value: val.value,
-							max: 0,
-							type: 'step',
-							_user: _id
-						});
-						let result = await step.save();
-					})
-					if (result) {
-						res.redirect('https://welse-node-v6.azurewebsites.net');
-					} else {
-						res.json({'message': 'error'});
-					}
+					const stepData = _.takeRight(steps, 100);
+					client.get('/activities/calories/date/today/1y.json', result.access_token)
+						.then( async (calRes) => {
+							const cal = calRes[0]['activities-calories'];
+							const calData = _.takeRight(cal, 100);
+
+							client.get('/activities/distance/date/today/1y.json', result.access_token)
+								.then( async (distanceRes) => {
+									const distance = distanceRes[0]['activities-distance'];
+									const disData = _.takeRight(distance, 100);
+									
+									_.forEach(stepData, async (val) => {
+										let date = new Date(val.dateTime).getTime() / 1000
+										let step = new Health({
+											chartType: 'column',
+											name: 'steps',
+											timestamp: date,
+											unit: 'step',
+											value: val.value,
+											max: 0,
+											type: 'step',
+											_user: _id
+										});
+										let stepResult = await step.save();
+									})
+
+									_.forEach(calData, async (val) => {
+										let date = new Date(val.dateTime).getTime() / 1000
+										let step = new Health({
+											chartType: 'column',
+											name: 'calories',
+											timestamp: date,
+											unit: 'calories',
+											value: val.value,
+											max: 0,
+											type: 'calories',
+											_user: _id
+										});
+										let calResult = await step.save();
+									})
+
+									_.forEach(disData, async (val) => {
+										let date = new Date(val.dateTime).getTime() / 1000
+										let step = new Health({
+											chartType: 'column',
+											name: 'distance',
+											timestamp: date,
+											unit: 'km',
+											value: val.value,
+											max: 0,
+											type: 'distance',
+											_user: _id
+										});
+										let disResult = await step.save();
+									})
+									if (disResult) {
+										res.redirect('https://welse-node-v6.azurewebsites.net');
+									} else {
+										res.json({'message': 'error'});
+									}
+								})
+						})
+
+
 				});
 		})
 		.catch( (error) => {
