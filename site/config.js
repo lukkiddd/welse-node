1456029bdb47f4eb9afed550be1049b8d4713217
xzust <k.kemiganithi@gmail.com>
2017-10-26 23:01:59 +0700

    change mongo config

--- site/config.js
+++ site/config.js
@@ -2,5 +2,5 @@ export default {
 	'SECRET_KEY': 'pMu5ljl~oZs5e_rdv*ibcFe}IYf64w9~[9r>>ia+IP1hDdbv87O4787z;67$<Vm7iP,=Pcjeq8M',
 	'CLIENT_ID': '22862W',
 	'CLIENT_SECRET': '3b7552c027a596a0688adbfaa20f28cb',
-	'DATABASE': 'mongodb://welse-cosmos:uQ0sDNGQHM7WjSVTvKPMuA9DhdKksAdjyq2CV1u74GxkYj1yCY8CU7ssw2vpfLrGrYVstedct7fhSBP01Euv6g==@welse-cosmos.documents.azure.com:10255/?ssl=true'
+	'DATABASE': 'mongodb://welse-cosmos-v1:8uthE5T7c0KtKdaklb2GmJkynvuGjXRkbsr5SSm4SSIh8oT6QqSG5eoXjdx2IMMTaoYRF5WR95aI6s5L1OmD2A==@welse-cosmos-v1.documents.azure.com:10255/?ssl=true'
 }
